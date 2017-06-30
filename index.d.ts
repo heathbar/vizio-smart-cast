083190821d86237092d102cf8c02f7e043770cf4
heathbar <hp@heathpaddock.com>
2017-06-30 02:20:06 -0500

    added typescript typings

---
+++ index.d.ts
@@ -0,0 +1,106 @@
+interface SmartcastStatic {
+    discover: (success: Function, error?: Function, timeout?: number) => void;
+    new(ip: string, authKey?: string): SmartcastStatic;
+    
+    power: SmartcastPower;
+    pairing: SmartcastPairing;
+    input: SmartcastInput;
+    control: SmartcastControl;
+    settings: SmartcastSettings;
+}
+
+interface SmartcastPower {
+    currentMode(): Promise<any>;
+}
+
+interface SmartcastPairing {
+    initialize(deviceName?: string, deviceId?: string): Promise<any>;
+    pair(pin: string): Promise<any>;
+    useAuthToken(token: string): void;
+}
+
+interface SmartcastInput {
+    current(): Promise<any>;
+    list(): Promise<any>;
+    set(name: string): Promise<any>;
+}
+
+interface SmartcastControl {
+    volume: SmartcastControlVolume;
+    input: SmartcastControlInput;
+    channel: SmartcastControlChannel;
+    power: SmartcastControlPower;
+    keyCommand(codeset: number, code: number, action?: string): Promise<any>;
+}
+
+interface SmartcastControlVolume {
+    up(): Promise<any>;
+    down(): Promise<any>;
+    mute(): Promise<any>;
+    unmute(): Promise<any>;
+    toggleMute(): Promise<any>;
+}
+
+interface SmartcastControlInput {
+    cycle(): Promise<any>;
+}
+
+interface SmartcastControlChannel {
+    up(): Promise<any>;
+    down(): Promise<any>;
+    previous(): Promise<any>;
+}
+
+interface SmartcastControlPower {
+    on(): Promise<any>;
+    off(): Promise<any>;
+    toggle(): Promise<any>;
+}
+
+interface SmartcastGet {
+    get(): Promise<any>;
+}
+
+interface SmartcastSettings {
+    picture: SmartcastSettingsPicture;
+    audio: SmartcastGet;
+    timers: SmartcastGet;
+    network: SmartcastGet;
+    channels: SmartcastGet;
+    closedCaptions: SmartcastGet;
+    devices: SmartcastGet;
+    system: SmartcastSettingsSystem;
+    mobileDevices: SmartcastGet;
+    cast: SmartcastGet;
+}
+
+interface SmartcastSettingsPicture {
+    get(): Promise<any>;
+    size: SmartcastGet;
+    position: SmartcastGet;
+    modeEdit: SmartcastGet;
+    color: SmartcastSettingsPictureColor;
+    calibrationTests: SmartcastGet;
+}
+
+interface SmartcastSettingsPictureColor {
+    calibration: SmartcastGet;
+    tuner: SmartcastGet;
+}
+
+interface SmartcastSettingsSystem {
+    get(): Promise<any>;
+    information: SmartcastSettingsSystemInformation;
+}
+
+interface SmartcastSettingsSystemInformation {
+    get(): Promise<any>;
+    tv: SmartcastGet;
+    tuner: SmartcastGet;
+    network: SmartcastGet;
+    uli: SmartcastGet;
+}
+
+declare var smartcast: SmartcastStatic;
+
+export = smartcast;
