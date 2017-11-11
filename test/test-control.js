07f3ab34b6557da959f88f5362eaa2fbb85200c5
Brian McNamara <brianmay27@gmail.com>
2017-11-10 16:02:19 -0800

    Update and add control tests

--- test/test-control.js
+++ test/test-control.js
@@ -7,214 +7,277 @@ let expect = require('chai').expect,
 
 describe('#smart-cast-control-tests', () => {
 
-    it('keyCommand should use KEYPRESS for default action', () => {
-        let tv = new smartcast('0.0.0.0'),
-            mockData = {};
+    let tv = new smartcast('0.0.0.0'),
+        mockData = {};
+    beforeEach(() => {
         sinon.stub(request, 'put').returns(Promise.resolve(mockData));
+    });
+    afterEach(() => {
+      request.put.restore();
+    });
 
+    it('keyCommand should use KEYPRESS for default action', () => {
         tv.control.keyCommand(0, 0);
-        
+
         expect(request.put.called).to.be.true;
         expect(request.put.firstCall.args[0].body.KEYLIST[0].ACTION).to.equal('KEYPRESS');
-
-        request.put.restore();
     });
 
     it('keyCommand should use specified parameters', () => {
-        let tv = new smartcast('0.0.0.0'),
-            mockData = {};
-        sinon.stub(request, 'put').returns(Promise.resolve(mockData));
 
         tv.control.keyCommand(1, 2, 'KEYDOWN');
-        
+
         expect(request.put.called).to.be.true;
         expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(1);
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(2);
         expect(request.put.firstCall.args[0].body.KEYLIST[0].ACTION).to.equal('KEYDOWN');
 
-        request.put.restore();
     });
 
 
     it('volume down should call api', () => {
-        let tv = new smartcast('0.0.0.0'),
-            mockData = {};
-        sinon.stub(request, 'put').returns(Promise.resolve(mockData));
-
         tv.control.volume.down();
-        
+
         expect(request.put.called).to.be.true;
         expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(5);
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(0);
 
-        request.put.restore();
     });
 
     it('volume up should call api', () => {
-        let tv = new smartcast('0.0.0.0'),
-            mockData = {};
-        sinon.stub(request, 'put').returns(Promise.resolve(mockData));
-
         tv.control.volume.up();
-        
+
         expect(request.put.called).to.be.true;
         expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(5);
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(1);
-
-        request.put.restore();
     });
 
-        it('mute off should call api', () => {
-        let tv = new smartcast('0.0.0.0'),
-            mockData = {};
-        sinon.stub(request, 'put').returns(Promise.resolve(mockData));
-
+    it('mute off should call api', () => {
         tv.control.volume.unmute();
-        
+
         expect(request.put.called).to.be.true;
         expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(5);
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(2);
-
-        request.put.restore();
     });
 
     it('mute on should call api', () => {
-        let tv = new smartcast('0.0.0.0'),
-            mockData = {};
-        sinon.stub(request, 'put').returns(Promise.resolve(mockData));
-
         tv.control.volume.mute();
-        
+
         expect(request.put.called).to.be.true;
         expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(5);
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(3);
-
-        request.put.restore();
     });
 
     it('mute toggle should call api', () => {
-        let tv = new smartcast('0.0.0.0'),
-            mockData = {};
-        sinon.stub(request, 'put').returns(Promise.resolve(mockData));
-
         tv.control.volume.toggleMute();
-        
+
         expect(request.put.called).to.be.true;
         expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(5);
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(4);
-
-        request.put.restore();
     });
 
     it('cycle input should call api', () => {
-        let tv = new smartcast('0.0.0.0'),
-            mockData = {};
-        sinon.stub(request, 'put').returns(Promise.resolve(mockData));
-
         tv.control.input.cycle();
-        
+
         expect(request.put.called).to.be.true;
         expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(7);
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(1);
-
-        request.put.restore();
     });
 
     it('channel down should call api', () => {
-        let tv = new smartcast('0.0.0.0'),
-            mockData = {};
-        sinon.stub(request, 'put').returns(Promise.resolve(mockData));
-
         tv.control.channel.down();
-        
+
         expect(request.put.called).to.be.true;
         expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(8);
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(0);
-
-        request.put.restore();
     });
 
     it('channel up should call api', () => {
-        let tv = new smartcast('0.0.0.0'),
-            mockData = {};
-        sinon.stub(request, 'put').returns(Promise.resolve(mockData));
-
         tv.control.channel.up();
-        
+
         expect(request.put.called).to.be.true;
         expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(8);
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(1);
-
-        request.put.restore();
     });
 
     it('previous channel should call api', () => {
-        let tv = new smartcast('0.0.0.0'),
-            mockData = {};
-        sinon.stub(request, 'put').returns(Promise.resolve(mockData));
-
         tv.control.channel.previous();
-        
+
         expect(request.put.called).to.be.true;
         expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(8);
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(2);
-
-        request.put.restore();
     });
 
     it('power off should call api', () => {
-        let tv = new smartcast('0.0.0.0'),
-            mockData = {};
-        sinon.stub(request, 'put').returns(Promise.resolve(mockData));
-
         tv.control.power.off();
-        
+
         expect(request.put.called).to.be.true;
         expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(11);
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(0);
-
-        request.put.restore();
     });
 
     it('power on should call api', () => {
-        let tv = new smartcast('0.0.0.0'),
-            mockData = {};
-        sinon.stub(request, 'put').returns(Promise.resolve(mockData));
-
         tv.control.power.on();
-        
+
         expect(request.put.called).to.be.true;
         expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(11);
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(1);
-
-        request.put.restore();
     });
 
     it('power toggle should call api', () => {
-        let tv = new smartcast('0.0.0.0'),
-            mockData = {};
-        sinon.stub(request, 'put').returns(Promise.resolve(mockData));
-
         tv.control.power.toggle();
-        
+
         expect(request.put.called).to.be.true;
         expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(11);
         expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(2);
+    });
 
-        request.put.restore();
+    it('Media seek forward should call api', () => {
+        tv.control.media.seek.forward();
+
+        expect(request.put.called).to.be.true;
+        expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(2);
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(0);
+    });
+
+    it('Media seek back should call api', () => {
+        tv.control.media.seek.back();
+
+        expect(request.put.called).to.be.true;
+        expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(2);
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(1);
+    });
+
+    it('Media play should call api', () => {
+        tv.control.media.play();
+
+        expect(request.put.called).to.be.true;
+        expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(2);
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(3);
+    });
+
+    it('Media pause should call api', () => {
+        tv.control.media.pause();
+
+        expect(request.put.called).to.be.true;
+        expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(2);
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(2);
+    });
+
+    it('Media cc should call api', () => {
+        tv.control.media.cc();
+
+        expect(request.put.called).to.be.true;
+        expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(4);
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(4);
+    });
+
+    it('Navigate up should call api', () => {
+        tv.control.navigate.up();
+
+        expect(request.put.called).to.be.true;
+        expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(3);
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(3);
+    });
+
+    it('Navigate down should call api', () => {
+        tv.control.navigate.down();
+
+        expect(request.put.called).to.be.true;
+        expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(3);
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(0);
+    });
+
+    it('Navigate left should call api', () => {
+        tv.control.navigate.left();
+
+        expect(request.put.called).to.be.true;
+        expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(3);
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(1);
+    });
+
+    it('Navigate right should call api', () => {
+        tv.control.navigate.right();
+
+        expect(request.put.called).to.be.true;
+        expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(3);
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(5);
+    });
+
+    it('Navigate ok should call api', () => {
+        tv.control.navigate.ok();
+
+        expect(request.put.called).to.be.true;
+        expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(3);
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(2);
+    });
+
+    it('Navigate back should call api', () => {
+        tv.control.navigate.back();
+
+        expect(request.put.called).to.be.true;
+        expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(4);
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(0);
+    });
+
+    it('Navigate exit should call api', () => {
+        tv.control.navigate.exit();
+
+        expect(request.put.called).to.be.true;
+        expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(9);
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(0);
+    });
+
+    it('Info should call api', () => {
+        tv.control.info();
+
+        expect(request.put.called).to.be.true;
+        expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(4);
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(6);
+    });
+
+    it('Menu should call api', () => {
+        tv.control.menu();
+
+        expect(request.put.called).to.be.true;
+        expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(4);
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(8);
+    });
+
+    it('SmartCast should call api', () => {
+        tv.control.smartcast();
+
+        expect(request.put.called).to.be.true;
+        expect(request.put.firstCall.args[0].url).to.equal('https://0.0.0.0:9000/key_command/');
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODESET).to.equal(4);
+        expect(request.put.firstCall.args[0].body.KEYLIST[0].CODE).to.equal(3);
     });
 
 });
